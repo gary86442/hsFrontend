b9c7d062025cd78b2490acba0a2a31f8e9d27d45
Wayne Sun <wayne10032004@gmail.com>
2023-07-26 21:59:24 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  publicPath: '/hsfrontend/signin' ,
   plugins: [
     vue(),
   ],
